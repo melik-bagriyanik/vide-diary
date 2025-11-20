1a6b874781a85374628e95d20a9c5bb307c6b420
melik-bagriyanik <melikbagriyanik90@gmail.com>
2025-11-20 16:05:08 +0300

    setup tanstack-query client provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,12 @@
+import { QueryClientProvider } from '@tanstack/react-query';
 import { Stack } from 'expo-router';
 
+import { queryClient } from '../src/lib/queryClient';
+
 export default function Layout() {
-  return <Stack />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Stack />
+    </QueryClientProvider>
+  );
 }
